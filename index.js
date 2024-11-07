e2068de7292cb748e595790cbcf09229655ee87a
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-06 20:48:03 -0700

    video and comment feature

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 8090;
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
--- routes/videos.js
+++ routes/videos.js
@@ -21,6 +21,7 @@ router.get('/videos', (req, res) => {
     }
   });
 });
+
 router.get('/videos/:videoId', (req, res) => {
   fs.readFile('data/videos.json', 'utf-8', (err, data) => {
     if (err) {
@@ -40,16 +41,68 @@ router.get('/videos/:videoId', (req, res) => {
   });
 });
 
-router.post('/:videoId/comments', (req, res) => {
+router.post('/video/:videoId', (req, res) => {
   console.log(req.body);
-  const { name, comment } = req.body;
-  const videoId = req.params.id;
+  fs.readFile('data/videos.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error reading data from file', err);
+      return res.status(500).json({ error: 'Error reading file' });
+    }
+    try {
+      const parsedVideos = JSON.parse(data);
+      const newVideo = {
+        id: uuidv4(),
+        title: req.body.title,
+        channel: 'Jenny Kim',
+        image:
+          'https://unit-3-project-api-0a5620414506.herokuapp.com/images/image8.jpg',
+        description: req.body.description,
+        views: '0',
+        likes: '0',
+        duration: '10:00',
+        video: 'https://unit-3-project-api-0a5620414506.herokuapp.com/stream',
+        timestamp: Date.now(),
+      };
+      parsedVideos.push(newVideo);
 
-  if (!name || !comment) {
-    return res.status(400).json({ error: 'Name and comment are required' });
-  }
+      fs.writeFileSync('./data/videos.json', JSON.stringify(parsedVideos));
+      res.status(201).send('you have created a new video!');
+    } catch (parseError) {
+      console.error('Error parsing data', parseError);
+      res.status(500).json({ error: 'Error parsing data' });
+    }
+  });
 });
 
-router.delete('/:videoId/comments/:commentId');
+router.post('/video/:videoId/comments', (req, res) => {
+  fs.readFile('data/videos.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error reading data from file', err);
+      return res.status(500).json({ error: 'Error reading file' });
+    }
+    if (req.body.id) {
+      try {
+        const addComments = JSON.parse(data);
+        const newComment = {
+          id: uuidv4(),
+          name: req.body.name,
+          comment: req.body.comment,
+          likes: 0,
+          timestamp: Date.now(),
+        };
+        addComments.push(newComment);
+        fs.writeFileSync('./data/videos.json', JSON.stringify(addComments));
+        res.status(201).send('you have created a new video!');
+      } catch (err) {
+        console.error('Error adding comment', err);
+        res.status(500).json({ error: 'Error adding comments' });
+      }
+    }
+  });
+});
+
+router.delete('/:videoId/comments/:commentId', (req, res) => {
+  res.send('hello this is delete!');
+});
 
 export default router;
